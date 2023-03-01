af83a44d1048ab293fa7fa52fa0a0a3581953ef3
kpodlaska <kpodlaska@griddynamics.com>
2023-03-01 17:35:37 +0100

    Primitive data types added in comments - Increment and Decrement Class added

---
+++ src/main/java/IncrementDecrementOperators/IncrementAndDecrementOperators.java
@@ -0,0 +1,65 @@
+package IncrementDecrementOperators;
+
+public class IncrementAndDecrementOperators {
+    public static void main(String[] args) {
+        System.out.println("Another file");
+        int number = 50;
+
+        // Display the value in number.
+        System.out.println("Number is " + number);
+
+        // Increment number.
+        number++;
+
+        // Display the value in number.
+        System.out.println("Now, number is " + number);
+
+        // Decrement number.
+        number--;
+
+        // Display the value in number.
+        System.out.println("Now, number is " + number);
+
+        //The example of the increment operator is:
+        //Pre-increment:
+        ++number;
+        System.out.println("Pre-increment: ++number");
+        System.out.println(number);
+        //Post-increment:
+        number++;
+        System.out.println("Post-increment: number++");
+        System.out.println(number);
+
+        //The example of the decrement operator is:
+        //Pre-increment:
+        --number;
+        System.out.println("Pre-increment: --number");
+        System.out.println(number);
+        //Post-increment:
+        number--;
+        System.out.println("Post-increment: number--");
+        System.out.println(number);
+        /*
+        Both the pre- and post-increment operators increment the value of the variable by 1.
+        Similarly, the pre- and post-decrement operators decrement the value of the variable by 1.
+        The difference becomes apparent when the variable using these operators is employed in an expression.
+        x = 5;
+        y = 10 + (++y);
+         */
+        int x=5, y=10;
+        int z = x * ++y;
+        System.out.println("z = z * ++y is "+ z);//55
+        int zz = x * y++;
+        System.out.println("zz = x * y++ is " + zz); //tez 55
+        //TODO znalezc java ludka by wyjasnil roznice bo ni chuja nie widze w kodzie a czuje ze bedzie na egzaminie
+        //jeszcze jedna proba
+        int d = 100;
+        int b = d++;
+        int c = ++d;
+        System.out.println("value of d is "+ d);//102
+        System.out.println("value of b is "+ b);//100 nie ma lipy bym to zrozumiale wytlumaczyla komus :D
+        System.out.println("value of c is "+ c);//102
+
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
         //Initialize the variable
         FirstVariable=5;
         System.out.println("First variable is :"+FirstVariable);
-        int SecondVariable=30;
+        int SecondVariable=-130;
         int total = FirstVariable + SecondVariable;
         System.out.println("Total is "+total);
         /* Primitive data types: Integer, Floating point numbers, character and boolean
@@ -33,7 +33,7 @@ public class Main {
         byte maxByte=127;
 
         //16 bit or 2 bytes
-        short minShort=--32_768;
+        short minShort=-32_768;
         short maxShort=32_767;
 
         //32 bit or 4 bytes
@@ -41,8 +41,56 @@ public class Main {
         int maxInt=2_147_483_647;
 
         //64 bit or 8 bytes
-        long minLong=-9_223_372_036_854_775_808;
-        long maxLong=9_223_372_036_854_775_807;
+        long minLong=-9_223_372_036_854_775_808L;
+        long maxLong=9_223_372_036_854_775_807L;
+        //********************************************************
+
+        //32 bit or 4 bytes
+        int myInt=10 / 3;
+        System.out.println("My int is "+myInt);
+
+        //32 bit or 4 bytes
+        float myFloat= (float) 10.25F / 3f; //need to by Fin Lne - by default every floating number is double
+        System.out.println("My float is "+myFloat);
+
+        //64 bit or 8 bytes
+        double myDouble=10.25 /3d;
+        System.out.println("My double is "+myDouble);
+
+        //1 bit boolean
+        boolean myBoolean = true;
+        System.out.println("My boolean is"+myBoolean);
+
+        /*TYPES OF JAVA OPERATORS
+        Arithmetic operators
+        + - * / & ++ --
+
+        Relational operators
+        == != < > <= =>
+
+        Logical operators
+        && || !
+
+        Assignment operators
+        = += -= *= /= %=
+
+        Conditional operators
+        ?:
+
+         */
+        int a=8, b=2, c=4;
+        System.out.println("a =" + a + ", b = "+b);
+        System.out.println("a + b ="+ (a+b));
+        System.out.println("a - b ="+ (a-b));
+        System.out.println("a * b ="+ (a*b));
+        System.out.println("a / b ="+ (a/b));
+        System.out.println("a & b ="+ (a&b));
+        //a++ is an equivalence of a = a + 1
+        System.out.println("print a "+a);
+        System.out.println("a++ ="+ a++ + " doesn't work");
+        System.out.println("a++ ="+ a + " does");
+        System.out.println("a-- ="+ (a--));
+
     }
 
 }
